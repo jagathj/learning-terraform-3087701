0336200e08de45b2adee5fc9d1e998421d2e76c9
Jag <44254268+jagathj@users.noreply.github.com>
2026-05-24 18:05:01 +1000

    env var update

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,7 +1,7 @@
 module "dev" {
     source = "../../modules/blog"
 
-    environment {
+    environment = {
         name            = "dev"
         network_prefix  = "10.1"
     }
--- environments/qa/main.tf
+++ environments/qa/main.tf
@@ -3,7 +3,7 @@ module "qa" {
 
     environment {
         name            = "qa"
-        network_prefix  = "10.1"
+        network_prefix  = "10.2"
     }
 
     min_size = 1
