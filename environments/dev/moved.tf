b55b6fc296cf04d919ca527a015b267e58a609f9
Jag <44254268+jagathj@users.noreply.github.com>
2026-05-24 16:29:08 +1000

    blog-dev

--- environments/dev/moved.tf
+++ environments/dev/moved.tf
@@ -14,12 +14,12 @@ moved {
 }
 
 moved {
-    from = aws_lb_target_group.blog
+    from = module.aws_lb_target_group.blog
     to   = module.dev.aws_lb_target_group.blog
 }
 
 moved {
-    from = module.autoscaling
-    to   = module.dev.module.autoscaling
+    from = module.blog_autoscaling
+    to   = module.dev.module.blog.autoscaling
 }
 
